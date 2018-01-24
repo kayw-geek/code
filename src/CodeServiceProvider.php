8ff50efb813ef4cfd73261ec80220cdac4da89bd
weikai <weikaiii@sina.cn>
2018-01-24 16:44:49 +0800

    '验证码'

---
+++ src/CodeServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Weikaiii\Code;
+
+use Illuminate\Support\ServiceProvider;
+
+class CodeServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton('code',function (){
+           return new Code();
+        });
+    }
+}
